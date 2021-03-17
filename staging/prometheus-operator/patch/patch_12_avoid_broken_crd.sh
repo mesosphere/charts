a36ccda60dfb25dd98a316219860f2e21ed9e28e
Joe Julian <me@joejulian.name>
2021-03-17 15:28:02 -0700

    update CRDs the d2iq way

--- staging/prometheus-operator/patch/patch_12_avoid_broken_crd.sh
+++ staging/prometheus-operator/patch/patch_12_avoid_broken_crd.sh
@@ -9,31 +9,93 @@ source "$(dirname "$0")/helpers.sh"
 
 set -xeuo pipefail
 
-SRCFILE="${BASEDIR}"/values.yaml
+mkdir -p "${BASEDIR}/files"
 
-sed -i '/^    alertmanagerConfigNamespaceSelector: {}$/d' "${SRCFILE}"
-sed -i '/^    alertmanagerConfigSelector: {}$/d' "${SRCFILE}"
-
-patch -d "${BASEDIR}" -p3 --no-backup-if-mismatch <<EOF
-diff --git a/staging/prometheus-operator/templates/alertmanager/alertmanager.yaml b/staging/prometheus-operator/templates/alertmanager/alertmanager.yaml
-index 1115e6145..9f0602e8d 100644
---- a/staging/prometheus-operator/templates/alertmanager/alertmanager.yaml
-+++ b/staging/prometheus-operator/templates/alertmanager/alertmanager.yaml
-@@ -47,14 +47,10 @@ spec:
- {{- if .Values.alertmanager.alertmanagerSpec.alertmanagerConfigSelector }}
-   alertmanagerConfigSelector:
- {{ toYaml .Values.alertmanager.alertmanagerSpec.alertmanagerConfigSelector | indent 4}}
--{{ else }}
--  alertmanagerConfigSelector: {}
- {{- end }}
- {{- if .Values.alertmanager.alertmanagerSpec.alertmanagerConfigNamespaceSelector }}
-   alertmanagerConfigNamespaceSelector:
- {{ toYaml .Values.alertmanager.alertmanagerSpec.alertmanagerConfigNamespaceSelector | indent 4}}
--{{ else }}
--  alertmanagerConfigNamespaceSelector: {}
- {{- end }}
- {{- if .Values.alertmanager.alertmanagerSpec.resources }}
-   resources:
+cat "${BASEDIR}"/crds/*.yaml > "${BASEDIR}/files/crds.yaml"
+cat <<EOF > "${BASEDIR}/templates/install_crds.yaml"
+{{ if not (.Capabilities.APIVersions.Has "prometheus.monitoring.coreos.com/v1") }}
+---
+{{ .Files.Get "files/crds.yaml" }}
+{{ end }}
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: {{ .Release.Namespace }}
+  name: prometheus-operator-crds
+  annotations:
+    "helm.sh/hook": pre-install, pre-upgrade
+    "helm.sh/hook-weight": "-5"
+    "helm.sh/hook-delete-policy": hook-succeeded,before-hook-creation
+data:
+  crds.yaml: {{ .Files.Get "files/crds.yaml" | toYaml | nindent 4 }}
+---
+apiVersion: batch/v1
+kind: Job
+metadata:
+  namespace: {{ .Release.Namespace }}
+  name: prometheus-operator-crds
+  annotations:
+    "helm.sh/hook": pre-install, pre-upgrade
+    "helm.sh/hook-weight": "-4"
+    "helm.sh/hook-delete-policy": hook-succeeded,before-hook-creation
+spec:
+  template:
+    spec:
+      serviceAccountName: prometheus-operator-crds
+      containers:
+        - name: prometheus-operator-crds
+          image: "bitnami/kubectl:1.19.7"
+          volumeMounts:
+            - name: prometheus-operator-crds
+              mountPath: /etc/prometheus-operator-crds
+              readOnly: true
+          command: ["kubectl", "apply", "-f", "/etc/prometheus-operator-crds"]
+      volumes:
+        - name: prometheus-operator-crds
+          configMap:
+            name: prometheus-operator-crds
+      restartPolicy: OnFailure
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: prometheus-operator-crds
+  annotations:
+    "helm.sh/hook": pre-install, pre-upgrade
+    "helm.sh/hook-weight": "-5"
+    "helm.sh/hook-delete-policy": hook-succeeded,before-hook-creation
+rules:
+  - apiGroups: ["apiextensions.k8s.io"]
+    resources: ["customresourcedefinitions"]
+    verbs: ["create", "get", "list", "watch", "patch"]
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: prometheus-operator-crds
+  annotations:
+    "helm.sh/hook": pre-install, pre-upgrade
+    "helm.sh/hook-weight": "-5"
+    "helm.sh/hook-delete-policy": hook-succeeded,before-hook-creation
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: prometheus-operator-crds
+subjects:
+  - kind: ServiceAccount
+    name: prometheus-operator-crds
+    namespace: {{ .Release.Namespace }}
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: prometheus-operator-crds
+  namespace: {{ .Release.Namespace }}
+  annotations:
+    "helm.sh/hook": pre-install, pre-upgrade
+    "helm.sh/hook-weight": "-5"
+    "helm.sh/hook-delete-policy": hook-succeeded,before-hook-creation
 EOF
 
 git_add_and_commit "${BASEDIR}"
