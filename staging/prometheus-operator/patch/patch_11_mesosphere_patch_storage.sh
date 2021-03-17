99fa9a67bea16dd7e8ec3053279507409beb9739
Joe Julian <me@joejulian.name>
2021-03-17 11:26:08 -0700

    do not create .orig files

--- staging/prometheus-operator/patch/patch_11_mesosphere_patch_storage.sh
+++ staging/prometheus-operator/patch/patch_11_mesosphere_patch_storage.sh
@@ -4,6 +4,6 @@ source $(dirname "$0")/helpers.sh
 
 set -xeuo pipefail
 
-patch -d "${BASEDIR}" -p3 < patch/mesosphere/patch/11_use_existing_storage_definitions.patch
+patch -d "${BASEDIR}" -p3 --no-backup-if-mismatch < patch/mesosphere/patch/11_use_existing_storage_definitions.patch
 
 git_add_and_commit "${BASEDIR}"/templates
