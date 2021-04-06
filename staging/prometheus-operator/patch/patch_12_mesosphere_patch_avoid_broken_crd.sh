a4676c2b6165c5af8014e52ff76b7816b8b81030
Grace Do <xgrace@gmail.com>
2021-04-05 20:55:11 -0700

    chore: Remove conditional patch

--- staging/prometheus-operator/patch/patch_12_mesosphere_patch_avoid_broken_crd.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source $(dirname "$0")/helpers.sh
-
-set -xeuo pipefail
-
-patch -d "${BASEDIR}" -p3 --no-backup-if-mismatch < patch/mesosphere/patch/12_conditional_avoid_broken_crd.patch
-
-git_add_and_commit "${BASEDIR}"/templates
