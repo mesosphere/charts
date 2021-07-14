d99018cb24195aba2d6ff043cbc18e0a2df0f301
Grace Do <xgrace@gmail.com>
2021-07-13 18:00:03 -0700

    chore: Bump kube-prometheus-stack tag to 16.13.0

--- staging/kube-prometheus-stack/upgrade_operator.sh
+++ staging/kube-prometheus-stack/upgrade_operator.sh
@@ -13,7 +13,7 @@ shopt -s dotglob
 BASEDIR=$(dirname "$(realpath "$0")")
 UPSTREAM_REPO=git@github.com:prometheus-community/helm-charts.git
 PROMETHEUS_PATH=charts/kube-prometheus-stack
-PROMETHEUS_TAG=kube-prometheus-stack-15.4.4
+PROMETHEUS_TAG=kube-prometheus-stack-16.13.0
 TMPDIR=$(mktemp -d)
 STARTING_REV=$(git rev-parse HEAD)
 export STARTING_REV
