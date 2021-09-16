158f639f7ad0810d6f50f9661328e23edd770444
Grace Do <xgrace@gmail.com>
2021-09-16 11:45:04 -0700

    chore: copy upstream chart version: 18.0.8

--- staging/kube-prometheus-stack/upgrade_operator.sh
+++ staging/kube-prometheus-stack/upgrade_operator.sh
@@ -13,7 +13,7 @@ shopt -s dotglob
 BASEDIR=$(dirname "$(realpath "$0")")
 UPSTREAM_REPO=git@github.com:prometheus-community/helm-charts.git
 PROMETHEUS_PATH=charts/kube-prometheus-stack
-PROMETHEUS_TAG=kube-prometheus-stack-17.2.1
+PROMETHEUS_TAG=kube-prometheus-stack-18.0.8
 TMPDIR=$(mktemp -d)
 STARTING_REV=$(git rev-parse HEAD)
 export STARTING_REV
