578a5e574b9263e488670556386a1f09690924df
Grace Do <xgrace@gmail.com>
2022-02-03 16:46:33 -0800

    fix: Bump grafana img to 7.5.6

--- staging/kube-prometheus-stack/patch/patch_7_mesosphere_default_values.sh
+++ staging/kube-prometheus-stack/patch/patch_7_mesosphere_default_values.sh
@@ -23,7 +23,6 @@ git_add_and_commit_with_msg ${SRCFILE} "reformat yaml with yq (no new changes)"
 # 1. For this version we set probeSelectorNilUsesHelmValues value to false to allow updates of crds from version 9 to 11
 # we should probably set to true for the next version bump
 
-
 docker run --rm -it \
   -v "${BASEDIR}":/basedir \
   -w /basedir \
@@ -31,5 +30,15 @@ docker run --rm -it \
   -e TMPFILE=${TMPFILE} \
   mikefarah/yq:3.3.2 \
   yq write -i "${SRCFILE}" prometheus.prometheusSpec.probeSelectorNilUsesHelmValues false
+
+# Set grafana image to v7.5.6
+
+docker run --rm -it \
+  -v "${BASEDIR}":/basedir \
+  -w /basedir \
+  -e SRCFILE=${SRCFILE} \
+  -e TMPFILE=${TMPFILE} \
+  mikefarah/yq:3.3.2 \
+  yq write -i "${SRCFILE}" grafana.image.tag 7.5.6
 
 git_add_and_commit_with_msg ${SRCFILE} "update values files"
