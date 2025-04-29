2b7b9c1e67c2fd7aa416c3fc4d84ee71a40fd42b
mohammad-irfan3 <mohammad.irfan@nutanix.com>
2025-04-29 16:47:58 +0530

    chore: copy upstream chart version: 71.0.0

--- staging/kube-prometheus-stack/upgrade_operator.sh
+++ staging/kube-prometheus-stack/upgrade_operator.sh
@@ -13,7 +13,7 @@ shopt -s dotglob
 BASEDIR=$(dirname "$(realpath "$0")")
 UPSTREAM_REPO=git@github.com:prometheus-community/helm-charts.git
 PROMETHEUS_PATH=charts/kube-prometheus-stack
-PROMETHEUS_TAG=kube-prometheus-stack-70.4.2
+PROMETHEUS_TAG=kube-prometheus-stack-71.0.0
 TMPDIR=$(mktemp -d)
 STARTING_REV=$(git rev-parse HEAD)
 export STARTING_REV
