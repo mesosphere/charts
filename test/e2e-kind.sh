d6aeed65add8d70da149b9c0dff4d434616135fc
kshitij-kumar-ngt <kshitij.kumar@nutanix.com>
2025-10-29 20:32:33 +0530

    fix: revert e2e kind script
    Signed-off-by: kshitij-kumar-ngt <kshitij.kumar@nutanix.com>

--- test/e2e-kind.sh
+++ test/e2e-kind.sh
@@ -176,16 +176,6 @@ install_elasticsearch() {
     docker_exec helm install elasticsearch --debug stable/elasticsearch
 }
 
-install_csi_driver() {
-    echo 'Installing latest csi driver...'
-    echo 'Add nutanix helm repository...'
-    docker_exec helm repo add ntnx-charts https://nutanix.github.io/helm-releases 
-    echo 'Install the csi-snapshot...'
-    docker_exec helm install nutanix-csi-snapshot ntnx-charts/nutanix-csi-snapshot -n ntnx-system --create-namespace
-    echo 'Install the csi driver...'
-    docker_exec helm install nutanix-csi-storage ntnx-charts/nutanix-csi-storage --namespace ntnx-system --set prismCentralEndPoint=127.0.0.1 --set pcUsername=dummy --set pcPassword='dummy' --set createSecret=false
-}
-
 replace_priority_class_name_system_x_critical() {
     # only change if needed
     set +o pipefail
@@ -206,7 +196,6 @@ main() {
     create_kind_cluster
     install_helm
     install_dummylb
-    install_csi_driver
     install_certmanager
     install_reloader
     
