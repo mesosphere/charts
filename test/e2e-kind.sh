7f8094c760d7fcc843cd5f92ac83ed318dd29da8
kshitij-kumar-ngt <kshitij.kumar@nutanix.com>
2025-10-29 13:31:04 +0530

    fix: installing dependencies for nutanix-csi
    Signed-off-by: kshitij-kumar-ngt <kshitij.kumar@nutanix.com>

--- test/e2e-kind.sh
+++ test/e2e-kind.sh
@@ -178,10 +178,10 @@ install_elasticsearch() {
 
 install_csi_driver() {
     echo 'Installing latest csi driver...'
-    echo 'Creating the ntnx-system namespace...'
-    docker_exec kubectl create namespace ntnx-system
     echo 'Add nutanix helm repository...'
     docker_exec helm repo add ntnx-charts https://nutanix.github.io/helm-releases 
+    echo 'Install the csi-snapshot...'
+    docker_exec helm install nutanix-csi-snapshot nutanix-helm/nutanix-csi-snapshot -n ntnx-system --create-namespace
     echo 'Install the csi driver...'
     docker_exec helm install nutanix-csi-storage ntnx-charts/nutanix-csi-storage --namespace ntnx-system --set prismCentralEndPoint=127.0.0.1 --set pcUsername=dummy --set pcPassword='dummy' --set createSecret=false
 }
